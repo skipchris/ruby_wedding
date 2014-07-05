caf372addf41a5633e4611fcd19b1c8e4b898e1d
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2014-07-05 20:34:17 +0100

    Add rspec config to allow describe "foo", :focus do

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = "random"
   config.color_enabled = true
 end
-
