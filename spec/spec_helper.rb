c3bcf9eaabf074332bde32a9e834eea2af2c94ec
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2014-07-06 20:30:48 +0100

    Run specs tagged with :focus by default
    Run all if no tag present.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,13 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = "random"
+
   config.color_enabled = true
+
   config.include FactoryGirl::Syntax::Methods
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 end
